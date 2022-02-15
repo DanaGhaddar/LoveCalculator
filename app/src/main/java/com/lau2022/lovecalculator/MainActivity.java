17aac1b0aec2203fbfa58ef14439453df7c4ea59
Dana Ghaddar <dana.ghaddar@lau.edu>
2022-02-15 23:22:48 +0200

    finishing the java code, adding images, and animations.

--- app/src/main/java/com/lau2022/lovecalculator/MainActivity.java
+++ app/src/main/java/com/lau2022/lovecalculator/MainActivity.java
@@ -6,21 +6,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
         public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+            int[] images = {R.drawable.python,R.drawable.javascript,R.drawable.java, R.drawable.cplus, R.drawable.csharp, R.drawable.php};
+            Spinner spinner;
+            String score = " ";
+            EditText PersonName;
+            TextView result;
+            TextView scoreboard;
+            ImageView imageView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.spinner1);
+        spinner = findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+
+        PersonName=findViewById(R.id.PersonName);
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imageoflang);
+        scoreboard = findViewById(R.id.scoreboard);
+
+
+
 
     }
 
@@ -35,4 +61,28 @@ import android.widget.Toast;
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
+
+
+
+            public void calculateLove(View v){
+                Random random = new Random();
+                int number = random.nextInt(99) + 1;
+
+                String chosenLang = spinner.getSelectedItem().toString();
+                String resultSentence = PersonName.getText() + " LOVES "+ chosenLang + " " + number + " %";
+                result.setText(resultSentence);
+
+                int itemId = (int) spinner.getSelectedItemId();
+                imageView.setImageResource(images[itemId]);
+                imageView.setVisibility(View.VISIBLE);
+                imageView.setAlpha(0f);
+                imageView.setTranslationY(50);
+                imageView.animate().alpha(1f).translationYBy(-50).setDuration(1500);
+
+
+                score += resultSentence + "\n";
+                scoreboard.setText(score);
+
+            }
+
         }
